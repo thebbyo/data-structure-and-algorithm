e94884fc6c39cce922ff69910eff2e80148fe49d
thebbyo <115203982+thebbyo@users.noreply.github.com>
2023-03-10 23:35:10 +0600

    Add files via upload

--- Solution.java
+++ Solution.java
@@ -1,28 +1,24 @@
 class Solution {
-    public static int search(int[] nums, int target) {
-        int lo = 0, hi = nums.length - 1;
-        while(hi - lo > 1){
+    public static int findMin(int[] nums) {
+        int lo = 0, hi = nums.length-1;
+        while (hi - lo > 1){
             int mid = (hi + lo)/2;
-            if(nums[mid] == target) return  mid;
-            //right sorted portion
-            if(nums[lo] <= nums[mid] ){
-                if(nums[mid] > target && nums[lo] <= target) hi = mid -1;
-                else lo = mid +1;
+            //left portion
+            if(nums[lo] <= nums[mid]){
+                if(nums[lo] > nums[hi]) lo = mid;
+                else hi = mid;
             }
-            // left sorted portion
             else {
-                if(nums[mid] < target && nums[hi] >= target) lo = mid +1;
-                else hi = mid-1;
+                if(nums[lo] <= nums[hi]) lo = mid;
+                else hi =  mid;
             }
         }
-        if(nums[lo] == target) return lo;
-        else if (nums[hi] == target) return hi;
-        else return -1;
+        if(nums[lo] > nums[hi]) return nums[hi];
+        else return nums[lo];
     }
 
     public static void main(String[] args) {
-        int[] nums = {5,6,7,8,1,2,3,4};
-        int target = 1;
-        System.out.println(search(nums, target));
+        int[] nums = {9,0,1,2,3,4,5,6,7,8};
+        System.out.println(findMin(nums));
     }
 }
